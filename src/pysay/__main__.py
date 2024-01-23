af3cc0c63e96e5781438f5066bc3fd697b60009a
Victor Payno <vpayno@users.noreply.github.com>
2024-01-23 07:27:36 -0800

    tests: add no cover pragmas to main entrypoints

--- src/pysay/__main__.py
+++ src/pysay/__main__.py
@@ -6,5 +6,5 @@ Entrypoint when running module: python -m pysay
 
 from pysay import main as cli
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     cli()
--- src/pysay/main.py
+++ src/pysay/main.py
@@ -19,5 +19,5 @@ def main():
     snake.say(" ".join(sys.argv[1:]))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
