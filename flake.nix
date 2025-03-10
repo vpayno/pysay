152656424ded477e2f86465939fa424a22b5ff41
Victor Payno <vpayno@users.noreply.github.com>
2025-03-09 21:18:04 -0700

    chore: format with nix fmt
    ```text
    $ nix fmt
    warning: Git tree '/home/vpayno/git_vpayno/pysay' is dirty
    2025/03/09 21:07:19 INFO using config file: /nix/store/q1sndm1zc30fx8wykvc8rbgch6j2czs3-treefmt.toml
    WARN no formatter for path: .envrc
    WARN no formatter for path: src/pysay/__init__.py
    WARN no formatter for path: src/pysay/__main__.py
    WARN no formatter for path: src/pysay/main.py
    WARN no formatter for path: src/pysay/snake.py
    WARN no formatter for path: tests/__init__.py
    WARN no formatter for path: tests/test_main.py
    WARN no formatter for path: tests/test_snake.py
    traversed 23 files
    emitted 8 files for processing
    formatted 4 files (4 changed) in 63ms
    ```

--- flake.nix
+++ flake.nix
@@ -22,211 +22,216 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    treefmt-conf ={
+    treefmt-conf = {
       url = "github:vpayno/nix-treefmt-conf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    pyproject-nix,
-    uv2nix,
-    pyproject-build-systems,
-    treefmt-conf,
-    ...
-  }: let
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      treefmt-conf,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
 
-    inherit (nixpkgs) lib;
+      inherit (nixpkgs) lib;
 
-    # Load a uv workspace from a workspace root.
-    # Uv2nix treats all uv projects as workspace projects.
-    workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+      # Load a uv workspace from a workspace root.
+      # Uv2nix treats all uv projects as workspace projects.
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-    # Create package overlay from workspace.
-    overlay = workspace.mkPyprojectOverlay {
-      # Prefer prebuilt binary wheels as a package source.
-      # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
-      # Binary wheels are more likely to, but may still require overrides for library dependencies.
-      sourcePreference = "wheel"; # or sourcePreference = "sdist";
-      # Optionally customise PEP 508 environment
-      # environ = {
-      #   platform_release = "5.10.65";
-      # };
-    };
-
-    # Extend generated overlay with build fixups
-    #
-    # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
-    # This is an additional overlay implementing build fixups.
-    # See:
-    # - https://pyproject-nix.github.io/uv2nix/FAQ.html
-    pyprojectOverrides = _final: _prev: {
-      # Implement build fixups here.
-      # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
-      # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
-    };
-
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    # Use Python 3.12 from nixpkgs
-    python = pkgs.python312;
-
-    # Construct package set
-    pythonSet =
-      # Use base package set from pyproject.nix builders
-      (pkgs.callPackage pyproject-nix.build.packages {
-        inherit python;
-      })
-      .overrideScope
-      (
-        lib.composeManyExtensions [
-          pyproject-build-systems.overlays.default
-          overlay
-          pyprojectOverrides
-        ]
-      );
-
-    metadata = {
-      homepage = "https://github.com/vpayno/pysay";
-      description = "Like cowsay but with a python";
-      license = with pkgs.lib.licenses; [mit];
-      # maintainers = with pkgs.lib.maintainers; [vpayno];
-      maintainers = {
-        email = "vpayno@users.noreply.github.com";
-        github = "vpayno";
-        githubId = 3181575;
-        name = "Victor Payno";
+      # Create package overlay from workspace.
+      overlay = workspace.mkPyprojectOverlay {
+        # Prefer prebuilt binary wheels as a package source.
+        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
+        # Binary wheels are more likely to, but may still require overrides for library dependencies.
+        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        # Optionally customise PEP 508 environment
+        # environ = {
+        #   platform_release = "5.10.65";
+        # };
       };
-      mainProgram = "pysay";
-    };
-  in {
-    formatter.${system} = treefmt-conf.formatter.${system};
 
-    # Package a virtual environment as our main application.
-    #
-    # Enable no optional dependencies for production build.
-    packages.${system}.default = pythonSet.mkVirtualEnv "pysay-prod-env" workspace.deps.default // {meta = metadata;};
+      # Extend generated overlay with build fixups
+      #
+      # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
+      # This is an additional overlay implementing build fixups.
+      # See:
+      # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+      pyprojectOverrides = _final: _prev: {
+        # Implement build fixups here.
+        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+      };
 
-    # Make pysay runnable with `nix run`
-    apps.${system} = {
-      default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/pysay";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Use Python 3.12 from nixpkgs
+      python = pkgs.python312;
+
+      # Construct package set
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+              pyprojectOverrides
+            ]
+          );
+
+      metadata = {
+        homepage = "https://github.com/vpayno/pysay";
+        description = "Like cowsay but with a python";
+        license = with pkgs.lib.licenses; [ mit ];
+        # maintainers = with pkgs.lib.maintainers; [vpayno];
+        maintainers = {
+          email = "vpayno@users.noreply.github.com";
+          github = "vpayno";
+          githubId = 3181575;
+          name = "Victor Payno";
+        };
+        mainProgram = "pysay";
+      };
+    in
+    {
+      formatter.${system} = treefmt-conf.formatter.${system};
+
+      # Package a virtual environment as our main application.
+      #
+      # Enable no optional dependencies for production build.
+      packages.${system}.default = pythonSet.mkVirtualEnv "pysay-prod-env" workspace.deps.default // {
         meta = metadata;
       };
-    };
 
-    # This example provides two different modes of development:
-    # - Impurely using uv to manage virtual environments
-    # - Pure development using uv2nix to manage virtual environments
-    devShells.${system} = {
-      # It is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages.
-      # This devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure.
-      impure = pkgs.mkShell {
-        packages = [
-          python
-          pkgs.uv
-        ];
-        env =
-          {
-            # Prevent uv from managing Python downloads
-            UV_PYTHON_DOWNLOADS = "never";
-            # Force uv to use nixpkgs Python interpreter
-            UV_PYTHON = python.interpreter;
-          }
-          // lib.optionalAttrs pkgs.stdenv.isLinux {
-            # Python libraries often load native shared objects using dlopen(3).
-            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-          };
-        shellHook = ''
-          unset PYTHONPATH
-        '';
+      # Make pysay runnable with `nix run`
+      apps.${system} = {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/pysay";
+          meta = metadata;
+        };
       };
 
-      # This devShell uses uv2nix to construct a virtual environment purely from Nix, using the same dependency specification as the application.
-      # The notable difference is that we also apply another overlay here enabling editable mode ( https://setuptools.pypa.io/en/latest/userguide/development_mode.html ).
-      #
-      # This means that any changes done to your local files do not require a rebuild.
-      #
-      # Note: Editable package support is still unstable and subject to change.
-      uv2nix = let
-        # Create an overlay enabling editable mode for all local dependencies.
-        editableOverlay = workspace.mkEditablePyprojectOverlay {
-          # Use environment variable
-          root = "$REPO_ROOT";
-          # Optional: Only enable editable for these packages
-          # members = [ "pysay" ];
-        };
-
-        # Override previous set with our overrideable overlay.
-        editablePythonSet = pythonSet.overrideScope (
-          lib.composeManyExtensions [
-            editableOverlay
-
-            # Apply fixups for building an editable package of your workspace packages
-            (final: prev: {
-              pysay = prev.pysay.overrideAttrs (old: {
-                # It's a good idea to filter the sources going into an editable build
-                # so the editable package doesn't have to be rebuilt on every change.
-                src = lib.fileset.toSource {
-                  root = old.src;
-                  fileset = lib.fileset.unions [
-                    (old.src + "/pyproject.toml")
-                    (old.src + "/README.md")
-                    (old.src + "/src/pysay/__init__.py")
-                  ];
-                };
-
-                # Hatchling (our build system) has a dependency on the `editables` package when building editables.
-                #
-                # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
-                # This behaviour is documented in PEP-660.
-                #
-                # With Nix the dependency needs to be explicitly declared.
-                nativeBuildInputs =
-                  old.nativeBuildInputs
-                  ++ final.resolveBuildSystem {
-                    editables = [];
-                  };
-              });
-            })
-          ]
-        );
-
-        # Build virtual environment, with local packages being editable.
-        #
-        # Enable all optional dependencies for development.
-        virtualenv = editablePythonSet.mkVirtualEnv "pysay-dev-env" workspace.deps.all;
-      in
-        pkgs.mkShell {
+      # This example provides two different modes of development:
+      # - Impurely using uv to manage virtual environments
+      # - Pure development using uv2nix to manage virtual environments
+      devShells.${system} = {
+        # It is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages.
+        # This devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure.
+        impure = pkgs.mkShell {
           packages = [
-            virtualenv
+            python
             pkgs.uv
           ];
-
-          env = {
-            # Don't create venv using uv
-            UV_NO_SYNC = "1";
-
-            # Force uv to use Python interpreter from venv
-            UV_PYTHON = "${virtualenv}/bin/python";
-
-            # Prevent uv from downloading managed Python's
-            UV_PYTHON_DOWNLOADS = "never";
-          };
-
+          env =
+            {
+              # Prevent uv from managing Python downloads
+              UV_PYTHON_DOWNLOADS = "never";
+              # Force uv to use nixpkgs Python interpreter
+              UV_PYTHON = python.interpreter;
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
           shellHook = ''
-            # Undo dependency propagation by nixpkgs.
             unset PYTHONPATH
-
-            # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
-            export REPO_ROOT=$(git rev-parse --show-toplevel)
           '';
         };
+
+        # This devShell uses uv2nix to construct a virtual environment purely from Nix, using the same dependency specification as the application.
+        # The notable difference is that we also apply another overlay here enabling editable mode ( https://setuptools.pypa.io/en/latest/userguide/development_mode.html ).
+        #
+        # This means that any changes done to your local files do not require a rebuild.
+        #
+        # Note: Editable package support is still unstable and subject to change.
+        uv2nix =
+          let
+            # Create an overlay enabling editable mode for all local dependencies.
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              # Use environment variable
+              root = "$REPO_ROOT";
+              # Optional: Only enable editable for these packages
+              # members = [ "pysay" ];
+            };
+
+            # Override previous set with our overrideable overlay.
+            editablePythonSet = pythonSet.overrideScope (
+              lib.composeManyExtensions [
+                editableOverlay
+
+                # Apply fixups for building an editable package of your workspace packages
+                (final: prev: {
+                  pysay = prev.pysay.overrideAttrs (old: {
+                    # It's a good idea to filter the sources going into an editable build
+                    # so the editable package doesn't have to be rebuilt on every change.
+                    src = lib.fileset.toSource {
+                      root = old.src;
+                      fileset = lib.fileset.unions [
+                        (old.src + "/pyproject.toml")
+                        (old.src + "/README.md")
+                        (old.src + "/src/pysay/__init__.py")
+                      ];
+                    };
+
+                    # Hatchling (our build system) has a dependency on the `editables` package when building editables.
+                    #
+                    # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
+                    # This behaviour is documented in PEP-660.
+                    #
+                    # With Nix the dependency needs to be explicitly declared.
+                    nativeBuildInputs =
+                      old.nativeBuildInputs
+                      ++ final.resolveBuildSystem {
+                        editables = [ ];
+                      };
+                  });
+                })
+              ]
+            );
+
+            # Build virtual environment, with local packages being editable.
+            #
+            # Enable all optional dependencies for development.
+            virtualenv = editablePythonSet.mkVirtualEnv "pysay-dev-env" workspace.deps.all;
+          in
+          pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+            ];
+
+            env = {
+              # Don't create venv using uv
+              UV_NO_SYNC = "1";
+
+              # Force uv to use Python interpreter from venv
+              UV_PYTHON = "${virtualenv}/bin/python";
+
+              # Prevent uv from downloading managed Python's
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+
+            shellHook = ''
+              # Undo dependency propagation by nixpkgs.
+              unset PYTHONPATH
+
+              # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+          };
+      };
     };
-  };
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 # shell.nix
 let
-  pkgs = import <nixpkgs> {};
-in let
-  packageOverrides = pkgs.callPackage ./python-packages.nix {};
-  python = pkgs.python3.override {inherit packageOverrides;};
-  pythonWithPackages = python.withPackages (ps: [ps.requests]);
+  pkgs = import <nixpkgs> { };
 in
-  pkgs.mkShell {
-    nativeBuildInputs = [pythonWithPackages];
-  }
+let
+  packageOverrides = pkgs.callPackage ./python-packages.nix { };
+  python = pkgs.python3.override { inherit packageOverrides; };
+  pythonWithPackages = python.withPackages (ps: [ ps.requests ]);
+in
+pkgs.mkShell {
+  nativeBuildInputs = [ pythonWithPackages ];
+}
